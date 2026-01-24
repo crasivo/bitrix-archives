6eee5da32e1f4e91bb1561a8bde2845100477242
Crasivo Dev <crasivodev@gmail.com>
2026-01-24 21:18:53 +0300

    Tmp changes

--- bin/download-distro.sh
+++ bin/download-distro.sh
@@ -13,7 +13,9 @@ set -o errexit
 S_FILEPATH=$(realpath "$0")
 S_CONTEXT_DIR=$(dirname "$S_FILEPATH")
 S_ROOT_DIR=$(realpath "$S_CONTEXT_DIR/../")
-S_TMP_DIR="$S_ROOT_DIR/tmp"
+
+# Options
+O_OUTPUT_DIR="$S_ROOT_DIR/tmp/distros"
 
 # GitHub
 GITHUB_REPO='crasivo/bitrix-archives'
@@ -48,22 +50,22 @@ BITRIX_MANIFEST_FILEPATH=${BITRIX_META_MANIFEST_FILEPATH:-}
 
 # @description Check release exists
 # @param $1 string Tag
-function _bx_check_release() {
-    echo "[INFO] Check release '$1'"
-    local http_status
-    http_status=$(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/repos/$GITHUB_REPO/releases/tags/$1")
-    if [[ $http_status == 404 ]]; then
-        echo "[INFO] Release '$1' not found. Continue..."
-        return
-    fi
+function _bx_check_release_tag_exists() {
+  echo "[INFO] Check release '$1'"
+  local http_status
+  http_status=$(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/repos/$GITHUB_REPO/releases/tags/$1")
+  if [[ $http_status == 404 ]]; then
+    echo "[INFO] Release '$1' not found. Continue..."
+    return
+  fi
 
-    # Notify Github
-    if [[ -n "$GITHUB_ENV" ]]; then
-        echo "SKIP_PUBLISH=true" >> "$GITHUB_ENV"
-    fi
+  # Notify Github
+  if [[ -n "$GITHUB_ENV" ]]; then
+    echo "SKIP_PUBLISH=true" >> "$GITHUB_ENV"
+  fi
 
-    # Stop execute
-    exit 0
+  # Stop execute
+  exit 0
 }
 
 # @description Создание финального manifest.json
@@ -87,10 +89,15 @@ function _bx_create_final_manifest() {
         bitrix: {
           distro_code: $distro_code,
           distro_type: $distro_type,
-          main_version: (if $main_version == "null" or $main_version == "" then null else $main_version end),
-          main_version_date: (if $main_version_date == "null" or $main_version_date == "" then null else $main_version_date end)
+          modules: [
+            {
+              id: "main",
+              version: (if $main_version == "null" or $main_version == "" then null else $main_version end),
+              version_date: (if $main_version_date == "null" or $main_version_date == "" then null else $main_version_date end)
+            }
+          ],
         },
-        artifacts: [
+        assets: [
           {
             filename: ($tar_filepath | split("/") | last),
             md5: $tar_md5,
@@ -113,103 +120,101 @@ function _bx_create_final_manifest() {
       }' > "$1"
 }
 
-# @description Extract main version from zip archive
-# @param $1 string Filepath (zip)
-function _bx_extract_main_version() {
-    # Extract PHP content
-    local php_content
-    php_content=$(unzip -p "$1" "bitrix/modules/main/classes/general/version.php")
-    if [[ -z "$php_content" ]]; then
-        echo "[WARN] Failed extract 'version.php' from '$1'"
-        return
-    fi
+# @description Извлечение информации о константах SM_VERSION
+# @param $1 string Input zip archive
+function _bx_extract_sm_version() {
+  # shellcheck disable=SC2155
+  local php_content=$(unzip -p "$1" "bitrix/modules/main/classes/general/version.php")
+  if [[ -z "$php_content" ]]; then
+    echo "[WARN] Failed to extract 'SM_VERSION' from '$1'"
+    return
+  fi
 
-    # Parse SM_VERSION from PHP content
-    local bx_version
-    bx_version=$(echo "$php_content" | grep -w "SM_VERSION" | awk -F "['\"]" '{print $4}')
-    if [[ $bx_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        BITRIX_MAIN_VERSION="$bx_version"
-        echo "[INFO] Main version: $BITRIX_MAIN_VERSION"
-    else
-        BITRIX_MAIN_VERSION=null
-        echo "[ERROR] Failed to parse 'SM_VERSION'."
-        return
-    fi
+  # shellcheck disable=SC2155
+  local sm_version=$(echo "$php_content" | grep -w "SM_VERSION" | awk -F "['\"]" '{print $4}')
+  if [[ $sm_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    BITRIX_MAIN_VERSION="$sm_version"
+    echo "[INFO] Main version: $BITRIX_MAIN_VERSION"
+  else
+    BITRIX_MAIN_VERSION=null
+    echo "[ERROR] Failed to parse 'SM_VERSION'."
+    return
+  fi
 
-    # Parse SM_VERSION_DATE from PHP content
-    local bx_date
-    bx_date=$(echo "$php_content" | grep -w "SM_VERSION_DATE" | awk -F "['\"]" '{print $4}')
-    if date -d "$bx_date" >/dev/null 2>&1; then
-        BITRIX_MAIN_VERSION_DATE=$(date -d "$bx_date" --iso-8601=seconds)
-        echo "[INFO] Main version date: $BITRIX_MAIN_VERSION_DATE"
-    else
-        BITRIX_MAIN_VERSION_DATE=null
-        echo "[WARN] Failed to parse 'SM_VERSION_DATE'."
-    fi
+  # shellcheck disable=SC2155
+  local sm_date=$(echo "$php_content" | grep -w "SM_VERSION_DATE" | awk -F "['\"]" '{print $4}')
+  if date -d "$sm_date" >/dev/null 2>&1; then
+    BITRIX_MAIN_VERSION_DATE=$(date -d "$sm_date" --iso-8601=seconds)
+    echo "[INFO] Main version date: $BITRIX_MAIN_VERSION_DATE"
+  else
+    BITRIX_MAIN_VERSION_DATE=null
+    echo "[WARN] Failed to parse 'SM_VERSION_DATE'."
+  fi
 }
 
 # @description Download archive
 # @param $1 string Output filepath
 # @param $2 string File extension
 function _bx_download_distro() {
-    if [[ -f $1 ]]; then
-        echo "[DEBUG] Local file $1 already exists"
-        return
-    fi
+  if [[ -f $1 ]]; then
+    echo "[DEBUG] Local file $1 already exists"
+    return
+  fi
 
-    # Define remote URL
-    local download_url
-    if [[ $BITRIX_DISTRO_TYPE == portal ]]; then
-        download_url="https://www.1c-bitrix.ru/download/portal/${BITRIX_DISTRO_CODE}_encode.$2"
-    else
-        download_url="https://www.1c-bitrix.ru/download/${BITRIX_DISTRO_CODE}_encode.$2"
-    fi
+  # Define remote URL
+  local download_url
+  if [[ $BITRIX_DISTRO_TYPE == portal ]]; then
+    download_url="https://www.1c-bitrix.ru/download/portal/${BITRIX_DISTRO_CODE}_encode.$2"
+  else
+    download_url="https://www.1c-bitrix.ru/download/${BITRIX_DISTRO_CODE}_encode.$2"
+  fi
 
-    # Execute
-    echo "[INFO] Download remote file: $download_url"
-    curl -SL -# "$download_url" -o "$1"
-    echo "[INFO] File was successfully downloaded: $1"
+  # Execute
+  echo "[INFO] Download remote file: $download_url"
+  curl -SL -# "$download_url" -o "$1"
+  echo "[INFO] File was successfully downloaded: $1"
 }
 
 # @description Dump file meta (tar)
 # @param $1 string Filepath
 function _bx_dump_tar_meta() {
-    echo "[INFO] Extract hashes - $1"
-    BITRIX_META_TAR_SIZE=$(stat -c%s "$1")
-    BITRIX_META_TAR_SIZE_MB=$(echo "scale=2; $BITRIX_META_TAR_SIZE / 1048576" | bc)
-    BITRIX_META_TAR_MD5=$(md5sum "$1" | awk '{ print $1 }')
-    BITRIX_META_TAR_SHA1=$(sha1sum "$1" | awk '{ print $1 }')
-    BITRIX_META_TAR_SHA256=$(sha256sum "$1" | awk '{ print $1 }')
-    # Dump
-    echo "$BITRIX_META_TAR_MD5" > "$1.md5"
-    echo "$BITRIX_META_TAR_SHA1" > "$1.sha1"
-    echo "$BITRIX_META_TAR_SHA256" > "$1.sha256"
+  echo "ℹ️ Extracting tar meta"
+  BITRIX_META_TAR_SIZE=$(stat -c%s "$1")
+  BITRIX_META_TAR_SIZE_MB=$(echo "scale=2; $BITRIX_META_TAR_SIZE / 1048576" | bc)
+  BITRIX_META_TAR_MD5=$(md5sum "$1" | awk '{ print $1 }')
+  BITRIX_META_TAR_SHA1=$(sha1sum "$1" | awk '{ print $1 }')
+  BITRIX_META_TAR_SHA256=$(sha256sum "$1" | awk '{ print $1 }')
+  # Dump
+  echo "$BITRIX_META_TAR_MD5" > "$1.md5"
+  echo "$BITRIX_META_TAR_SHA1" > "$1.sha1"
+  echo "$BITRIX_META_TAR_SHA256" > "$1.sha256"
 }
 
 # @description Dump file meta (zip)
 # @param $1 string Filepath
 function _bx_dump_zip_meta() {
-    echo "[INFO] Extract hashes - $1"
-    BITRIX_META_ZIP_SIZE=$(stat -c%s "$1")
-    BITRIX_META_ZIP_SIZE_MB=$(echo "scale=2; $BITRIX_META_ZIP_SIZE / 1048576" | bc)
-    BITRIX_META_ZIP_MD5=$(md5sum "$1" | awk '{ print $1 }')
-    BITRIX_META_ZIP_SHA1=$(sha1sum "$1" | awk '{ print $1 }')
-    BITRIX_META_ZIP_SHA256=$(sha256sum "$1" | awk '{ print $1 }')
-    # Dump
-    echo "$BITRIX_META_ZIP_MD5" > "$1.md5"
-    echo "$BITRIX_META_ZIP_SHA1" > "$1.sha1"
-    echo "$BITRIX_META_ZIP_SHA256" > "$1.sha256"
+  echo "ℹ️ Extracting zip meta"
+  BITRIX_META_ZIP_SIZE=$(stat -c%s "$1")
+  BITRIX_META_ZIP_SIZE_MB=$(echo "scale=2; $BITRIX_META_ZIP_SIZE / 1048576" | bc)
+  BITRIX_META_ZIP_MD5=$(md5sum "$1" | awk '{ print $1 }')
+  BITRIX_META_ZIP_SHA1=$(sha1sum "$1" | awk '{ print $1 }')
+  BITRIX_META_ZIP_SHA256=$(sha256sum "$1" | awk '{ print $1 }')
+  # Dump
+  echo "$BITRIX_META_ZIP_MD5" > "$1.md5"
+  echo "$BITRIX_META_ZIP_SHA1" > "$1.sha1"
+  echo "$BITRIX_META_ZIP_SHA256" > "$1.sha256"
 }
 
 # @description Export variables (github action)
 function _bx_export_github_variables() {
-    if [[ -z "$GITHUB_ENV" ]]; then
-        echo "[DEBUG] Skip export GitHub variables"
-        return
-    fi
-    for var in $(compgen -v | grep '^BITRIX_'); do
-        echo "$var=${!var}" >> "$GITHUB_ENV"
-    done
+  echo "⬆️ Export variables (GitHub Action)"
+  if [[ -z "$GITHUB_ENV" ]]; then
+    echo "Skipping..."
+    return
+  fi
+  for var in $(compgen -v | grep '^BITRIX_'); do
+    echo "$var=${!var}" >> "$GITHUB_ENV"
+  done
 }
 
 # ----------------------------------------------------------------
@@ -217,32 +222,32 @@ function _bx_export_github_variables() {
 # ----------------------------------------------------------------
 
 function _cmd_process() {
-    local output_dir="$S_TMP_DIR/$BITRIX_DISTRO_TYPE/$BITRIX_DISTRO_CODE"
-    mkdir -p "$output_dir"
+  local output_dir="$O_OUTPUT_DIR/$BITRIX_DISTRO_TYPE/$BITRIX_DISTRO_CODE"
+  mkdir -p "$output_dir"
 
-    # Step 1: Process ZIP archive
-    BITRIX_META_ZIP_FILEPATH="$output_dir/${BITRIX_DISTRO_CODE}_encode.zip"
-    _bx_download_distro "$BITRIX_META_ZIP_FILEPATH" "zip"
-    _bx_dump_zip_meta "$BITRIX_META_ZIP_FILEPATH"
-    _bx_extract_main_version "$BITRIX_META_ZIP_FILEPATH"
-    if [[ -z "$BITRIX_MAIN_VERSION" ]] || [[ $BITRIX_MAIN_VERSION == null ]]; then
-        echo "[ERROR] Failed to extract main version"
-        exit 1
-    fi
+  # Step 1: Process ZIP archive
+  BITRIX_META_ZIP_FILEPATH="$output_dir/${BITRIX_DISTRO_CODE}_encode.zip"
+  _bx_download_distro "$BITRIX_META_ZIP_FILEPATH" "zip"
+  _bx_dump_zip_meta "$BITRIX_META_ZIP_FILEPATH"
+  _bx_extract_sm_version "$BITRIX_META_ZIP_FILEPATH"
+  if [[ -z "$BITRIX_MAIN_VERSION" ]] || [[ $BITRIX_MAIN_VERSION == null ]]; then
+    echo "[ERROR] Failed to extract main version"
+    exit 1
+  fi
 
-    # Step 2: Check release
-    if [[ -z "$GITHUB_RELEASE_TAG" ]]; then
-        _bx_check_release "GITHUB_RELEASE_TAG"
-    fi
+  # Step 2: Check release
+  if [[ -z "$GITHUB_RELEASE_TAG" ]]; then
+    _bx_check_release_tag_exists "GITHUB_RELEASE_TAG"
+  fi
 
-    # Step 3: Process TAR archive
-    BITRIX_META_TAR_FILEPATH="$output_dir/${BITRIX_DISTRO_CODE}_encode.tar.gz"
-    _bx_download_distro "$BITRIX_META_TAR_FILEPATH" "tar.gz"
-    _bx_dump_tar_meta "$BITRIX_META_TAR_FILEPATH"
+  # Step 3: Process TAR archive
+  BITRIX_META_TAR_FILEPATH="$output_dir/${BITRIX_DISTRO_CODE}_encode.tar.gz"
+  _bx_download_distro "$BITRIX_META_TAR_FILEPATH" "tar.gz"
+  _bx_dump_tar_meta "$BITRIX_META_TAR_FILEPATH"
 
-    # Step 4: Create manifest
-    BITRIX_MANIFEST_FILEPATH="$output_dir/manifest.json"
-    _bx_create_final_manifest "$BITRIX_MANIFEST_FILEPATH"
+  # Step 4: Create manifest
+  BITRIX_MANIFEST_FILEPATH="$output_dir/manifest.json"
+  _bx_create_final_manifest "$BITRIX_MANIFEST_FILEPATH"
 }
 
 # ----------------------------------------------------------------
@@ -251,41 +256,45 @@ function _cmd_process() {
 
 # Check arguments
 if [[ "$#" -lt 1 ]]; then
-    echo "[ERROR] Illegal number of parameters"
-    exit 1
+  echo "❌ Error: Illegal number of parameters"
+  exit 1
 fi
 
 # Define distro
 case "$1" in
-    start|standard|business|small_business|business_cluster|business_cluster_postgresql)
-        BITRIX_DISTRO_CODE=$1
-        BITRIX_DISTRO_TYPE=web
-        shift
-        ;;
-    bitrix24|bitrix24_shop|bitrix24_enterprise)
-        BITRIX_DISTRO_CODE=$1
-        BITRIX_DISTRO_TYPE=portal
-        shift
-        ;;
-    *)
-        echo "[ERROR] Undefined action ($1)"
-        exit 1
-        ;;
+  start|standard|business|small_business|business_cluster|business_cluster_postgresql)
+    BITRIX_DISTRO_CODE=$1
+    BITRIX_DISTRO_TYPE=web
+    shift
+    ;;
+  bitrix24|bitrix24_shop|bitrix24_enterprise)
+    BITRIX_DISTRO_CODE=$1
+    BITRIX_DISTRO_TYPE=portal
+    shift
+    ;;
+  *)
+    echo "[ERROR] Undefined action ($1)"
+    exit 1
+    ;;
 esac
 
 # Parse options
 for i in "$@"; do
-    case "$i" in
-        --env=*)
-            # shellcheck disable=SC2046
-            export $(grep -E '^#' "${i#*=}" | xargs)
-            shift
-            ;;
-        --type=*)
-            BITRIX_DISTRO_TYPE="${i#*=}"
-            shift
-            ;;
-    esac
+  case "$i" in
+    --env=*)
+      # shellcheck disable=SC2046
+      export $(grep -E '^#' "${i#*=}" | xargs)
+      shift
+      ;;
+    --type=*)
+      BITRIX_DISTRO_TYPE="${i#*=}"
+      shift
+      ;;
+    --output=*)
+      O_OUTPUT_DIR="${i#*=}"
+      shift
+      ;;
+  esac
 done
 
 # Process
---
+++ bin/download-script.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o errtrace
+set -o nounset
+set -o errexit
+
+# ----------------------------------------------------------------
+# Environments
+# ----------------------------------------------------------------
+
+# Context (constant)
+S_FILEPATH=$(realpath "$0")
+S_CONTEXT_DIR=$(dirname "$S_FILEPATH")
+S_ROOT_DIR=$(realpath "$S_CONTEXT_DIR/../")
+
+# Options
+O_OUTPUT_DIR="$S_ROOT_DIR/tmp"
+
+# GitHub
+GITHUB_REPO='crasivo/bitrix-archives'
+GITHUB_ENV=${GITHUB_ENV:-}
+GITHUB_TOKEN=${GITHUB_TOKEN:-}
+GITHUB_RELEASE_DATE=${GITHUB_RELEASE_DATE:-}
+GITHUB_RELEASE_TAG=${GITHUB_RELEASE_TAG:-}
+
+# Bitrix: General
+BITRIX_SCRIPT_NAME=${BITRIX_SCRIPT_NAME:-}
+BITRIX_MANIFEST_PATH=${BITRIX_MANIFEST_PATH:-}
+
+# Bitrix: Meta
+BITRIX_META_SCRIPT_PATH=${BITRIX_META_SCRIPT_PATH:-}
+BITRIX_META_SCRIPT_SIZE=${BITRIX_META_SCRIPT_SIZE:-}
+BITRIX_META_SCRIPT_MD5=${BITRIX_META_SCRIPT_MD5:-}
+BITRIX_META_SCRIPT_SHA1=${BITRIX_META_SCRIPT_SHA1:-}
+BITRIX_META_SCRIPT_SHA256=${BITRIX_META_SCRIPT_SHA256:-}
+
+# ----------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------
+
+function _bx_create_final_manifest() {
+  echo "🧾 Create manifest.json"
+  jq -n \
+    --arg script_path  "$BITRIX_META_SCRIPT_PATH" \
+    --arg script_size  "$BITRIX_META_SCRIPT_SIZE" \
+    --arg script_md5  "$BITRIX_META_SCRIPT_MD5" \
+    --arg script_sha1  "$BITRIX_META_SCRIPT_SHA1" \
+    --arg script_sha256  "$BITRIX_META_SCRIPT_SHA256" \
+    --arg release_date  "$GITHUB_RELEASE_DATE" \
+    --arg release_tag  "$GITHUB_RELEASE_TAG" \
+    '{
+      release_date: $release_date,
+      release_tag: $release_tag,
+      assets: [
+        {
+          filename: ($script_path | split("/") | last),
+          md5: $script_md5,
+          sha1: $script_sha1,
+          sha256: $script_sha256,
+          size: ($script_size | tonumber)
+        }
+      ]
+    }' > "$1"
+}
+
+# @description Download PHP script
+# @param $1 string Output dir
+# @param $2 string Filename
+function _bx_download_script() {
+  local download_url="https://www.1c-bitrix.ru/download/scripts/$2"
+  echo "⬇️ Downloading PHP script ($download_url)..."
+  if [[ -f "$1/$2" ]]; then
+    echo "File $1 already exists. Skipping."
+    return
+  fi
+
+  curl -sSL "$download_url" -o "$1/$2"
+}
+
+# @description Dump file meta (bitrixsetup.php)
+# @param $1 string Filepath
+function _bx_dump_script_meta() {
+  echo "ℹ️ Extracting file meta"
+  BITRIX_META_SCRIPT_SIZE=$(stat -c%s "$1")
+  BITRIX_META_SCRIPT_SIZE_MB=$(echo "scale=2; $BITRIX_META_SCRIPT_SIZE / 1024" | bc)
+  BITRIX_META_SCRIPT_MD5=$(md5sum "$1" | awk '{ print $1 }')
+  BITRIX_META_SCRIPT_SHA1=$(sha1sum "$1" | awk '{ print $1 }')
+  BITRIX_META_SCRIPT_SHA256=$(sha256sum "$1" | awk '{ print $1 }')
+  # Dump
+  echo "$BITRIX_META_SCRIPT_MD5" > "$1.md5"
+  echo "$BITRIX_META_SCRIPT_SHA1" > "$1.sha1"
+  echo "$BITRIX_META_SCRIPT_SHA256" > "$1.sha256"
+}
+
+# @description Export variables (github action)
+function _bx_export_github_variables() {
+  echo "⬆️ Export variables (GitHub Action)"
+  if [[ -z "$GITHUB_ENV" ]]; then
+    echo "Skipping..."
+    return
+  fi
+  for var in $(compgen -v | grep '^BITRIX_'); do
+    echo "$var=${!var}" >> "$GITHUB_ENV"
+  done
+}
+
+# ----------------------------------------------------------------
+# Commands
+# ----------------------------------------------------------------
+
+# @description Run process
+function _cmd_run() {
+  local output_dir="$O_OUTPUT_DIR/$BITRIX_SCRIPT_NAME"
+  mkdir -p "$output_dir"
+
+  # Step 1. Download script
+  BITRIX_META_SCRIPT_PATH="$output_dir/$BITRIX_SCRIPT_NAME"
+  _bx_download_script "$output_dir" "$BITRIX_SCRIPT_NAME"
+  _bx_dump_script_meta "$BITRIX_META_SCRIPT_PATH"
+
+  # Step 2. Create manifest.json
+  BITRIX_MANIFEST_PATH="$output_dir/manifest.json"
+  _bx_create_final_manifest "$BITRIX_MANIFEST_PATH"
+}
+
+# ----------------------------------------------------------------
+# Runtime
+# ----------------------------------------------------------------
+
+# Check arguments
+if [[ "$#" -lt 1 ]]; then
+  echo "❌ Error: Illegal number of parameters"
+  exit 1
+fi
+
+# Define script (action)
+if [[ $1 = *.php ]]; then
+  BITRIX_SCRIPT_NAME="$1"
+  shift
+else
+  echo "❌ Error: Unknown action ($1)"
+  exit 1
+fi
+
+# Parse options
+for i in "$@"; do
+  case "$i" in
+    --env=*)
+      # shellcheck disable=SC2046
+      export $(grep -E '^#' "${i#*=}" | xargs)
+      shift
+      ;;
+    --output=*)
+      O_OUTPUT_DIR="${i#*=}"
+      shift
+      ;;
+  esac
+done
+
+# Run process
+_cmd_run
+
+# Export variables (github)
+_bx_export_github_variables
